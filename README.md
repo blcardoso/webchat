1ed0748ee68dc1e5920aded3020534d698d46c46
Bruno Cardoso <bruno.cardosox@gmail.com>
2023-02-22 12:33:21 -0300

    add source files

---
+++ README.md
@@ -0,0 +1,33 @@
+# Nuxt 3 + Vuetify 3
+## Setup
+
+Dependências:
+
+```bash
+# yarn
+yarn install
+
+# npm
+npm install
+
+# pnpm
+pnpm install
+```
+
+## Server local
+
+```bash
+npm run dev
+```
+
+## Produção
+
+```bash
+npm run build
+```
+
+Preview da produção
+
+```bash
+npm run preview
+```
---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    css: ['vuetify/lib/styles/main.sass'],
+    build: {
+        transpile: ['vuetify'],
+    },
+    vite: {
+        define: {
+            'process.env.DEBUG': false,
+        },
+    },
+    modules: [
+        '@pinia/nuxt'
+    ],
+    pinia: {
+        autoImports: [
+            'defineStore'
+        ],
+    }
+})
